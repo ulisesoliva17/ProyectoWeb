1615a10018057d078cecf0412aed98009d3714e1
Agustin Alonso <agustin.alonso@est.fi.uncoma.edu.ar>
2025-10-07 17:51:15 -0300

    conexion json

--- ml.js
+++ ml.js
@@ -27,3 +27,29 @@ function crearCard(titulo, parrafos, contenedorId) { //Definimos la funcion.
         console.warn(`No existe el contenedor con id "${contenedorId}"`);
     }
 }
+
+async function cargarCards() {  //Declara una función asíncrona llamada cargarCards, 
+                               //lo que permite usar await dentro de ella para manejar 
+                              // operaciones que tardan (como leer archivos externos)
+  
+    try {
+    //Usa fetch para pedir el archivo externo cards.json.
+    //await espera a que la respuesta llegue antes de continuar. 
+    const respuesta = await fetch("cards.json"); //Lo  tengo que cambiar por el json al final vamos a usar
+    
+    //Convierte la respuesta en formato JSON a un objeto JavaScript.
+    //datos será un array de objetos, cada uno representando una tarjeta.
+    const datos = await respuesta.json();
+
+    datos.forEach(item => {  
+        crearCard(item.titulo, item.parrafos, item.categoria);
+    });
+  } catch (error) {
+    console.error("Error al cargar las tarjetas:", error);
+  }
+}
+
+// Ejecutar al cargar la página
+document.addEventListener("DOMContentLoaded", cargarCards);//Espera a que el HTML esté completamente cargado.
+                                                          //Una vez listo, ejecuta la función cargarCards para iniciar el proceso.
+
